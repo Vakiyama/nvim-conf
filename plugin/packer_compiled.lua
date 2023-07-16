124fa4eb21c859f780cfd432a5687920cf6bbaea
Vakiyama <vakiyama@outlook.com>
2023-07-16 15:49:58 -0700

    added trouble plugin for better dealing with errors

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -185,6 +185,11 @@ _G.packer_plugins = {
     path = "/root/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["trouble.nvim"] = {
+    loaded = true,
+    path = "/root/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    url = "https://github.com/folke/trouble.nvim"
+  },
   undotree = {
     loaded = true,
     path = "/root/.local/share/nvim/site/pack/packer/start/undotree",
